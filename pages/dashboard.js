0892ea9d6bfca8c11403b5b05fad8886ddd93c6b
Redtag2619 <sas2619@me.com>
2026-02-25 22:11:44 -0600

    Create dashboard.js

---
+++ pages/dashboard.js
@@ -0,0 +1,11 @@
+export default function Dashboard() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Consultant Dashboard</h1>
+
+      <a href="/search">Search Candidates</a>
+      <br /><br />
+      <a href="/admin">Admin Panel</a>
+    </div>
+  );
+}
