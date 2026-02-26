bc43590a7a36d4486f1bf6238d47e3635154f5fb
Redtag2619 <sas2619@me.com>
2026-02-25 23:12:06 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,9 @@ const partySelect = document.getElementById("partySelect") as HTMLSelectElement;
 const resultsDiv = document.getElementById("results")!;
 const paginationDiv = document.getElementById("pagination")!;
 
+let currentPage = 1;
+console.log(currentPage);
+
 const limit = 12;
 
 /* ==========================
