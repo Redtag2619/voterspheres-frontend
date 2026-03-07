131e456cbedfb0178bbe4a94a88412c8752ed5d4
Redtag2619 <sas2619@me.com>
2026-03-07 11:22:08 -0600

    Create FundraisingDashboard.jsx

---
+++ src/pages/FundraisingDashboard.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { Bar } from "react-chartjs-2"
+
+export default function FundraisingDashboard(){
+
+const [data,setData] = useState([])
+
+useEffect(()=>{
+
+axios.get("https://voterspheres-backend-2pap.onrender.com/money/fundraising")
+.then(res=>{
+setData(res.data.fundraising)
+})
+
+},[])
+
+const chartData = {
+
+labels: data.map(d=>d.candidate),
+
+datasets:[
+{
+label:"Fundraising",
+data:data.map(d=>d.total_fundraising)
+}
+]
+
+}
+
+return(
+
+<div style={{padding:"30px"}}>
+
+<h1>Real-Time Fundraising</h1>
+
+<Bar data={chartData}/>
+
+</div>
+
+)
+
+}
