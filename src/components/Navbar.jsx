d7e1f97ed5df53cebb3bc4904fac75277f5f254f
Redtag2619 <sas2619@me.com>
2026-03-07 08:55:24 -0600

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+
+export default function Navbar() {
+  return (
+    <div style={{background:"#0f172a", padding:"16px", color:"white"}}>
+
+      <h2>VoterSpheres Intelligence</h2>
+
+      <div style={{display:"flex", gap:"20px", marginTop:"10px"}}>
+
+        <Link to="/">Dashboard</Link>
+        <Link to="/candidates">Candidates</Link>
+        <Link to="/map">Election Map</Link>
+        <Link to="/donors">Donor Network</Link>
+        <Link to="/warroom">AI War Room</Link>
+
+      </div>
+
+    </div>
+  )
+}
