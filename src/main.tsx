1af22406ef944c58eb39a2fb12a51a18eacc5edd
Redtag2619 <sas2619@me.com>
2026-03-07 16:05:20 -0600

    Update and rename main.ts to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-
-// Dark intelligence theme
 import "./styles/theme.css"
 
 ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 ).render(
-
   <React.StrictMode>
     <App />
   </React.StrictMode>
-
 )
