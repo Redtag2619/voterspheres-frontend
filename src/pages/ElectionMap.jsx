bb84a559b3ac56ab7506de930fe26c6dda2f5d70
Redtag2619 <sas2619@me.com>
2026-03-07 09:04:41 -0600

    Create ElectionMap.jsx

---
+++ src/pages/ElectionMap.jsx
@@ -0,0 +1,15 @@
+export default function ElectionMap(){
+
+return(
+
+<div style={{padding:"30px"}}>
+
+<h1>National Campaign Map</h1>
+
+<p>Interactive election map coming here.</p>
+
+</div>
+
+)
+
+}
