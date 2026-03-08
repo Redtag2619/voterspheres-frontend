a774c2a9393447b554bd9808800cc5207d40ecde
Redtag2619 <sas2619@me.com>
2026-03-08 13:38:34 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import "./styles/theme.css"
+```tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
+```
