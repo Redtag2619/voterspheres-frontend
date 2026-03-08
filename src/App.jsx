af50080fdd0b3f2cc85198529eae9ccd8bf203da
Redtag2619 <sas2619@me.com>
2026-03-08 15:01:40 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-
 import Dashboard from "./pages/Dashboard";
 import Candidates from "./pages/Candidates";
 import ElectionMap from "./pages/ElectionMap";
@@ -20,7 +19,6 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/candidates" element={<Candidates />} />
