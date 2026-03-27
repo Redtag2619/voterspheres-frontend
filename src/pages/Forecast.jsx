d6f87647233b29accc32f178841631740e3a5f3d
Redtag2619 <sas2619@me.com>
2026-03-27 16:43:10 -0500

    Update Forecast.jsx

--- src/pages/Forecast.jsx
+++ src/pages/Forecast.jsx
@@ -1,6 +1,6 @@
 import { useAuth } from "../context/AuthContext";
 import { hasPlan } from "../lib/plan";
-import UpgradeGate from "../components/UpgradeGate";
+import UpgradeGate from "../components/UpgradeGate"; 
 
 export default function Forecast() {
   const { user } = useAuth();
