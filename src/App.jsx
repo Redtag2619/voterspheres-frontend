976dcc570adf582950ebb58bfc19d0bb271419dd
Redtag2619 <sas2619@me.com>
2026-03-29 11:16:31 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense, lazy } from "react"; 
 import { Routes, Route } from "react-router-dom"; 
 
 import AppShell from "./app/layout/AppShell";
