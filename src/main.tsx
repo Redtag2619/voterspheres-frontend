aecfae49848da9e9af95103c588570cd60acd755
Redtag2619 <sas2619@me.com>
2026-03-29 11:05:04 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { UpgradePromptProvider } from "./context/UpgradePromptContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <UpgradePromptProvider>
+          <App />
+        </UpgradePromptProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
