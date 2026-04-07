a6e5e36db4029bedcd92118235bffc8a38c6bd83
Redtag2619 <sas2619@me.com>
2026-04-07 14:10:13 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles/theme.css";
 import ErrorBoundary from "./components/ErrorBoundary.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <BrowserRouter>
+      <AuthProvider>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
