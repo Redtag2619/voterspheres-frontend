7131bce6476ecdb568a61194fd3f6dbb3d20be37
Redtag2619 <sas2619@me.com>
2026-05-14 17:39:25 -0500

    Update CampaignOpportunityHeatmap.jsx

--- src/pages/CampaignOpportunityHeatmap.jsx
+++ src/pages/CampaignOpportunityHeatmap.jsx
@@ -7,7 +7,7 @@ import {
 } from "react-simple-maps";
 import { api } from "../services/api";
 import PageShell from "../components/ui/PageShell";
-import SectionCard from "../components/ui/SectionCard";
+import SectionCard from "../components/ui/SectionCard"; 
 import StatCard from "../components/ui/StatCard";
 import Badge from "../components/ui/Badge";
 import EmptyState from "../components/ui/EmptyState";
