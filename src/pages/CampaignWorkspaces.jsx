e0bbb633dc5d28d621511eee8144d2726edfd24e
Redtag2619 <sas2619@me.com>
2026-06-01 20:13:16 -0500

    Update CampaignWorkspaces.jsx

--- src/pages/CampaignWorkspaces.jsx
+++ src/pages/CampaignWorkspaces.jsx
@@ -7,6 +7,7 @@ import StatCard from "../components/ui/StatCard";
 import Badge from "../components/ui/Badge";
 import EmptyState from "../components/ui/EmptyState";
 import ResponsiveRow from "../components/ui/ResponsiveRow";
+import { useRealtimeTacticalEvents } from "../hooks/useRealtimeTacticalEvents";
 
 function fmtNumber(value) {
   return Number(value || 0).toLocaleString();
