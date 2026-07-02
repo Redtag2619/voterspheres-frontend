dbecc30f58fc10d0d6d0fa2ef57e19df6f6a4d20
Redtag2619 <sas2619@me.com>
2026-07-02 16:25:46 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Suspense, lazy } from "react";
-import { BrowserRouter, Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom"; 
+import { BrowserRouter, Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";  
 import ErrorBoundary from "./components/ErrorBoundary.jsx"; 
 import VirtualTour from "./components/VirtualTour.jsx";
 import AppShell from "./components/AppShell";
