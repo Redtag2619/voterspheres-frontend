97e7265ea6e1ff2145ae32cdf22167af916676e3
Redtag2619 <sas2619@me.com>
2026-07-30 15:13:12 -0500

    Update LiveIntelligenceLayer.jsx

--- src/pages/LiveIntelligenceLayer.jsx
+++ src/pages/LiveIntelligenceLayer.jsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useMemo, useState } from "react"; 
 import { Link } from "react-router-dom";
-import { api } from ".. /services/api"; 
+import { api } from "../services/api"; 
 
 import PageShell from ".. /components/ui/PageShell"; 
 import SectionCard from ".. /components/ui/SectionCard"; 
